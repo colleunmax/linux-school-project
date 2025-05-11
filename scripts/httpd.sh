f71548cfd52ce289bcc950c305341023843cd5f6
colleunmax <mr.collejoulian@gmail.com>
2025-05-11 14:08:02 +0200

    fix: removing gum from the httpd.sh script

--- scripts/httpd.sh
+++ scripts/httpd.sh
@@ -2,28 +2,33 @@
 CONFIG_FILE_PATH="/etc/httpd/conf/httpd.conf"
 HOSTNAME="localhost"
 
-function fn_setup_httpd() {
+function fn_main() {
+    # installing httpd and mod_ssl
     dnf install -y httpd mod_ssl
-    systemctl enable httpd --now
-}
 
-function fn_setup_self_signed_ssl() {
+    # starting httpd.service
+    systemctl enable httpd --now
+
+    # Setup self-signed ssl
     openssl genrsa -out "$HOSTNAME" 2048
     openssl req -new -key "$HOSTNAME" -out "$HOSTNAME".csr -sha512
     openssl x509 -req -days 365 -in "$HOSTNAME".csr -signkey "$HOSTNAME".key -out "$HOSTNAME".crt -sha512
     cp "$HOSTNAME".crt /etc/pki/tls/certs/
     mv "$HOSTNAME".csr /etc/pki/tls/private/"$HOSTNAME".csr
     mv "$HOSTNAME".key /etc/pki/tls/private/"$HOSTNAME".key
-}
 
-function fn_setup_hostname() {
-    HOSTNAME=$(gum input --placeholder="eg: localhost" --header="Update l'hostname")
+    # Setup hostname
+    HOSTNAME="Localhost"
+    read -p "Update l'hostname (eg: localhost): " HOSTNAME
     hostnamectl set-hostname "$HOSTNAME"
+
+    export -f fn_setup_httpd
+    export -f fn_setup_self_signes_ssl
 }
 
-export -f fn_setup_httpd
-export -f fn_setup_self_signes_ssl
 
-fn_setup_hostname
-gum spin --spinner dot --title "Installing httpd service and other depedencies..." -- bash -c 'fn_setup_httpd'
-gum spin --spinner dot --title "Installing httpd service and other depedencies..." -- bash -c 'fn_setup_self_signed_ssl'
+if [ $EUID -eq "0" ]; then
+    sudo fn_main
+else
+    exec sudo bash "$0" "$@"
+fi
