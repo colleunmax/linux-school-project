225729ab6ebcc381a9b5e092f09365fdd631f732
colleunmax <mr.collejoulian@gmail.com>
2025-05-16 10:33:02 +0200

    edit dir perms

--- scripts/lamp/remove-user-httpd.sh
+++ scripts/lamp/remove-user-httpd.sh
@@ -13,4 +13,23 @@ sudo sed -i "/<VirtualHost \*:80>/,/<\/VirtualHost>/ {
     /ServerName $username\.website\.lan/,\%<\/VirtualHost>%d
 }" "$config_file"
 
+mkdir -p "/home/$username/public_html"
+chown -R "$username:$username" "/home/$username/public_html"
+
+chmod o+x /home
+chmod o+x "/home/$username"
+chmod o+rx "/home/$username/public_html"
+
+find "/home/$username/public_html" -type f -exec chmod o+r {} \;
+find "/home/$username/public_html" -type d -exec chmod o+rx {} \;
+
+echo "✅ /home/$username/public_html is now Apache-accessible and still owned by $username"
+
+
+
+
+
+
+
+
 echo "🗑️ VirtualHost for $username.website.lan removed from $config_file"
