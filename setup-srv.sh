4f44372e7f6507d822df9e8b0f4b8b4d8712afb6
colleunmax <mr.collejoulian@gmail.com>
2025-05-16 09:05:10 +0200

    feat: adding global srv setup script

--- setup-srv.sh
+++ setup-srv.sh
@@ -1,5 +1,18 @@
 IP=$(ip -4 addr show ens5 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
 
-source ./scripts/lvm/lvm-setup-srv.sh
-source ./scripts/dns/setup-dns.sh $IP
-source ./scripts/lamp/setup-lamp.sh
+fn_main() {
+    source ./scripts/lvm/lvm-setup-srv.sh
+    source ./scripts/dns/setup-dns.sh $IP
+    source ./scripts/lamp/setup-lamp.sh
+    source ./scripts/nfs/nfs-server-setup.sh
+    source ./scripts/samba/samba-setup.sh
+    source ./scripts/ftp/ftp-install.sh
+    source ./scripts/monitoring/monitoring.sh
+}
+
+
+if [ $EUID -eq "0" ]; then
+  fn_main
+else
+  exec sudo bash "$0" "$@"
+fi
